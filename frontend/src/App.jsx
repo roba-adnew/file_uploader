aeae0a8971b41d0babbfc503a678e84c1a423f15
roba <10408936+roba-adnew@users.noreply.github.com>
2024-08-02 11:09:50 -0700

    set up navbar with react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
+import { Outlet } from 'react-router-dom'
 import './App.css'
+import NavBar from './Components/NavBar'
 
 function App() {
 
   return (
-    <div>
-      sup
-    </div>
+    <>
+      <NavBar />
+      <Outlet />
+    </>
   )
 }
 
