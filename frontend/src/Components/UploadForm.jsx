6a20929ace04dcc432add351614ab6b3c731e429
roba <10408936+roba-adnew@users.noreply.github.com>
2024-08-09 11:51:43 -0400

    Implemented auth-gated form on frontend

--- frontend/src/Components/UploadForm.jsx
+++ frontend/src/Components/UploadForm.jsx
@@ -1,8 +1,28 @@
+import { useContext } from "react";
+import { AuthContext } from "../Contexts/AuthContext";
+
 function UploadForm() {
+    const { isAuthorized } = useContext(AuthContext)
+
     return (
+        isAuthorized ? 
         <div>
-            Form to come
+            <form action="/stats" encType="multipart/form-data" method="post">
+                <div className="form-group">
+                    <input
+                        type="file"
+                        className="form-control-file"
+                        name="uploaded_file"
+                    />
+                    <input
+                        type="submit"
+                        value="upload"
+                        className="upload btn"
+                    />
+                </div>
+            </form>
         </div>
+        : <div>please login to play</div>
     )
 }
 
