92e4d0bbbbcb16242f1c222017bcdc4904bd271a
roba <10408936+roba-adnew@users.noreply.github.com>
2024-08-11 13:44:42 -0400

    fixed multer upload with absolute path

--- backend/src/controllers/managerContr.js
+++ backend/src/controllers/managerContr.js
@@ -4,7 +4,7 @@ const multer = require('multer')
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        return cb(null, "../../public")
+        return cb(null, 'public')
     },
     filename: (req, file, cb) => {
         return cb(null, file.originalname) 
@@ -16,11 +16,8 @@ const upload = multer({ storage })
 exports.formUploadPost = [
     upload.single('uploaded_file'),
     (req, res, next) => {
-        debug('made it to form upload'); 
-        debug('form', req.body)
-        debug('file', req.file)
-        next()},
-    (req, res, next) => {
+        debug('file details', req.file)
         if (!req.isAuthenticated()) return res.status(401)
+        next()
     }
 ]
