00f3a0bbcf729b711dbf07043558955779a85bd1
roba <10408936+roba-adnew@users.noreply.github.com>
2024-08-13 16:57:16 -0400

    resolving merge conflict

--- backend/src/routes/manage.js
+++ backend/src/routes/manage.js
@@ -7,11 +7,7 @@ router.get('/file', managerContr.viewFileGet)
 
 router.post('/file', managerContr.fileUploadPost)
 
-<<<<<<< HEAD
 router.put('/file', managerContr.updateFileNamePut)
-=======
-router.get('/file', managerContr.viewFileGet)
->>>>>>> 1656b0d715d3458325f0ff2ff029c80fec588aa3
 
 router.post('/folder', managerContr.createFolderPost)
 
